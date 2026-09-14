f63f69013d8d6bac4c122fbbabe96b3ffdf174d9
Manav9910ms <manav991077@gmail.com>
2026-09-14 18:29:29 +0530

    Fix signin branding asset

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import AuthCard from '@/components/auth-card';
 
+const brandIcon = 'https://raw.githubusercontent.com/Manav9910ms/nowmywork/main/icon.png';
+
 export default function SignInPage() {
   return (
     <main className="auth-page">
       <Link href="/" className="auth-brand">
-        <img src="/icon.png" alt="" />
+        <img src={brandIcon} alt="" />
         <span>NowMyWork</span>
       </Link>
       <AuthCard mode="signin" />
